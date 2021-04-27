fbf68bd00c5a4465db2527be2db63a096dc7aa5f
nikolas-virionis <nikolas.virionis@bandtec.com.br>
2021-04-27 12:27:14 -0300

    template superior e lateral 90% pronto

--- script.js
+++ script.js
@@ -1,23 +1,31 @@
-let setInter, timer = 0;
-leftsidebarButton.setAttribute('onclick', 'openNav();');
+let setInter, timer = 0, menu = document.querySelector(".menu-btn");
 document.addEventListener('mousemove', () => {
-    leftsidebarButton.style.opacity = "1";
+    menu.style.opacity = "1";
     document.documentElement.style.cursor = 'auto';
     document.body.style.filter = "brightness(1)";
     clearInterval(setInter);
     timer = 0;
-    leftsidebarButton.setAttribute('onclick', 'openNav();');
     setInter = setInterval(dimInterval, 1000);
 })
-const openNav = () => document.getElementById("myLeftSidenav").style.width = "25vw";
-const closeNav = () => document.getElementById("myLeftSidenav").style.width = "0";
 const dimInterval = () => {
-    if (timer <= 10) timer++;
+    if (timer < 30) timer++;
     else {
-        leftsidebarButton.style.opacity = "0";
-        leftsidebarButton.removeAttribute('onclick');
-        document.body.style.filter = "brightness(0.5)";
+        menu.style.opacity = "0";
+        document.body.style.filter = "brightness(0.75)";
         document.documentElement.style.cursor = 'none';
     }
 }
 setInter = setInterval(dimInterval, 1000);
+const menuBtn = document.querySelector('.menu-btn');
+let menuOpen = false;
+menuBtn.addEventListener('click', () => {
+  if(!menuOpen) {
+    menuBtn.classList.add('open');
+    document.getElementById("myLeftSidenav").style.width = "25vw";
+    menuOpen = true;
+  } else {
+    menuBtn.classList.remove('open');
+    document.getElementById("myLeftSidenav").style.width = "0";
+    menuOpen = false;
+  }
+});
