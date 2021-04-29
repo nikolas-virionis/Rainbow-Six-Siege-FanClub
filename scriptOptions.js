1867544bf152c77a35811d09aaf7af8f90ac6104
nikolas-virionis <nikolas.virionis@bandtec.com.br>
2021-04-29 20:11:53 -0300

    delete historia/superação.html + menuOptions 100%

--- scriptOptions.js
+++ scriptOptions.js
@@ -1,2 +1,13 @@
+let confirmLogOut;
 menuOptions.style.display = "none";
-historyTab.style.marginTop = "12.5vh"
+historyTab.style.marginTop = "12.5vh";
+const btnOptions = link => {
+    window.location.href = link;
+}
+const logOut = () => {
+    confirmLogOut = confirm('Tem certeza que deseja fazer log out?');
+    if (confirmLogOut) {
+        sessionStorage.removeItem('logins');
+        window.location.href = "login.html";
+    }
+}
