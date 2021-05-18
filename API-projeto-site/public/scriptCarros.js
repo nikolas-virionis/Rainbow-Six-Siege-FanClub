3e1d1b1928fed44cc8d0107985c20877cd7fc50f
nikolas-virionis <nikolas.virionis@bandtec.com.br>
2021-05-18 20:53:29 -0300

    backup dados carros / local²

--- API-projeto-site/public/scriptCarros.js
+++ API-projeto-site/public/scriptCarros.js
@@ -8,7 +8,65 @@ else if (document.title[document.title.length - 1].toLowerCase() == "r") passwor
 else if (document.title[document.title.length - 1].toLowerCase() == "i") passwordId.value = 6;
 else if (document.title[document.title.length - 1].toLowerCase() == "k") passwordId.value = 7;
 else console.error("Erro de escolha do carro, tente novamente mais tarde")
-form_carro.submit()
+
+// form_carro.submit();
+
+if (passwordId.value == "1") {
+    vMaxId.innerHTML = 320;
+    aceleração0_100Id.innerHTML = 1.99;
+    pesoId.innerHTML = 2162;
+    powerId.innerHTML = 1020;
+    chargeRangeId.innerHTML = 624;
+    aceleração400mId.innerHTML = 9.23;
+} 
+else if (passwordId.value == "2") {
+    vMaxId.innerHTML = 260;
+    aceleração0_100Id.innerHTML = 3.1;
+    pesoId.innerHTML = 1844;
+    powerId.innerHTML = 480;
+    chargeRangeId.innerHTML = 504;
+    aceleração400mId.innerHTML = 11.60;
+}
+else if (passwordId.value == "3") {
+    vMaxId.innerHTML = 261;
+    aceleração0_100Id.innerHTML = 2.5;
+    pesoId.innerHTML = 2445;
+    powerId.innerHTML = 1020;
+    chargeRangeId.innerHTML = 544;
+    aceleração400mId.innerHTML = 9.90;
+}
+else if (passwordId.value == "4") {
+    vMaxId.innerHTML = 248;
+    aceleração0_100Id.innerHTML = 3.5;
+    pesoId.innerHTML = 2003;
+    powerId.innerHTML = 456;
+    chargeRangeId.innerHTML = 485;
+    aceleração400mId.innerHTML = 12.40;
+}
+else if (passwordId.value == "5") {
+    vMaxId.innerHTML = 400;
+    aceleração0_100Id.innerHTML = '<1.9';
+    pesoId.innerHTML = 1305;
+    powerId.innerHTML = 1020;
+    chargeRangeId.innerHTML = 1000;
+    aceleração400mId.innerHTML = 8.80;
+}
+else if (passwordId.value == "6") {
+    vMaxId.innerHTML = 130;
+    aceleração0_100Id.innerHTML = 20;
+    pesoId.innerHTML = 11340;
+    powerId.innerHTML = 1000;
+    chargeRangeId.innerHTML = 810;
+}
+else if (passwordId.value == "7") {
+    vMaxId.innerHTML = 200;
+    aceleração0_100Id.innerHTML = 4.5;
+    pesoId.innerHTML = 2950;
+    powerId.innerHTML = 800;
+    chargeRangeId.innerHTML = 480;
+    aceleração400mId.innerHTML = 10.80;
+}
+
 function checkCarro() {
     var formularioCarro = new URLSearchParams(new FormData(form_carro));
     fetch("/carros/autenticarCarro", {
@@ -26,12 +84,12 @@ function checkCarro() {
                 vMaxId.innerHTML = vMax;
                 aceleração0_100Id.innerHTML = aceleração0_100;
                 pesoId.innerHTML = peso;
-                potenciaId.innerHTML = potencia;
+                powerId.innerHTML = potencia;
                 chargeRangeId.innerHTML = chargeRange;
                 if(aceleração400mId) aceleração400mId.innerHTML = aceleração400m;
             });
         } else {
-            alert('Erro ao carregar informações sobre o carro \n Tente novamente mais tarde');
+            // alert('Erro ao carregar informações sobre o carro \n Tente novamente mais tarde');
             // window.location.href = "menuOptions.html";
         }
     });
