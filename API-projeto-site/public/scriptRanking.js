49e9378cdb81dcc3fa6d75ba5d86f671a489b453
nikolas-virionis <nikolas.virionis@bandtec.com.br>
2021-05-24 09:49:21 -0300

    continuação debug(não finalizado) rank

--- API-projeto-site/public/scriptRanking.js
+++ API-projeto-site/public/scriptRanking.js
@@ -1,7 +1,42 @@
-var varIdCarro, 
-arrayCarros = ['Model S', 'Model 3', 'Model X', 'Model Y', 'Roadster', 'Semi', 'CyberTruck'], 
-arrayObjs = [];
+var arrayCarros = ['Model S', 'Model 3', 'Model X', 'Model Y', 'Roadster', 'Semi', 'CyberTruck'], 
+arrayObjs = [
+    {
+        id: 1,
+        nomeCarro: "Model S",
+        qtdFavs: 0
+    },
+    {
+        id: 2,
+        nomeCarro: 'Model 3',
+        qtdFavs: 0
+    },
+    {
+        id: 3,
+        nomeCarro: 'Model X',
+        qtdFavs: 0
+    },
+    {
+        id: 4,
+        nomeCarro: 'Model Y',
+        qtdFavs: 0
+    },
+    {
+        id: 5,
+        nomeCarro: 'Roadster',
+        qtdFavs: 0
+    },
+    {
+        id: 6,
+        nomeCarro: 'Semi',
+        qtdFavs: 0
+    },
+    {
+        id: 7,
+        nomeCarro: 'Cybertruck',
+        qtdFavs: 0
+    }];
 emailId.value = 1;
+submitRank();
 function submitRank() {
     for (let varIdCarro = 1; varIdCarro <= 7; varIdCarro++){
         emailId.value = varIdCarro;
@@ -9,22 +44,17 @@ function submitRank() {
         fetch("/usuarios/autenticarRank", {
             method: "POST",
             body: formulario
-        }).then(resposta => {
-            if (resposta.ok) {
-                resposta.json().then(json => {
-                    for (let iterator of arrayObjs) {
-                        if(iterator.id == Number(json.id)) iterator.qtdFavs = Number(json.resultado);
-                    }
-                });
-            } else {
-                console.error(`Erro de autenticação dos carro ${varIdCarro}`)
-                alert('Username ou senha incorretos');
-                resposta.text().then(texto => {
-                    console.warn(texto);
-                });
-            }
-        });
+        }).then(resposta => resposta.ok 
+            ? resposta.json().then(json => arrayObjs[varIdCarro - 1].id == Number(json.id) 
+                ? arrayObjs[varIdCarro - 1].qtdFavs = Number(json.resultado) 
+                : '') 
+            : console.error(`Erro de autenticação dos carro ${varIdCarro}`));
     }
+    console.log(arrayObjs)
+    assignValues();
+return false;
+}
+function assignValues() {
     firstLink.innerText = arrayObjs[0].nomeCarro;
     secondLink.innerText = arrayObjs[1].nomeCarro;
     thirdLink.innerText = arrayObjs[2].nomeCarro;
@@ -39,44 +69,4 @@ function submitRank() {
     ptsFifth.innerHTML = arrayObjs[4].qtdFavs;
     ptsSixth.innerHTML = arrayObjs[5].qtdFavs;
     ptsSeventh.innerHTML = arrayObjs[6].qtdFavs;
-return false;
 }
-
-arrayObjs.push({
-    id: 1,
-    nomeCarro: "Model S",
-    qtdFavs: 0
-},
-{
-    id: 2,
-    nomeCarro: 'Model 3',
-    qtdFavs: 0
-},
-{
-    id: 3,
-    nomeCarro: 'Model X',
-    qtdFavs: 0
-},
-{
-    id: 4,
-    nomeCarro: 'Model Y',
-    qtdFavs: 0
-},
-{
-    id: 5,
-    nomeCarro: 'Roadster',
-    qtdFavs: 0
-},
-{
-    id: 6,
-    nomeCarro: 'Semi',
-    qtdFavs: 0
-},
-{
-    id: 7,
-    nomeCarro: 'Cybertruck',
-    qtdFavs: 0
-});
-    arrayObjs[0].nomeCarro = "Model S";
-    console.log(arrayObjs[0].qtdFavs);
-submitRank()
