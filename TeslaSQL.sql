a233731d283b59798b25d50f5c7b4c21e62593be
nikolas-virionis <nikolas.virionis@bandtec.com.br>
2021-05-24 13:12:28 -0300

    criação do script sql

---
+++ TeslaSQL.sql
@@ -0,0 +1,32 @@
+CREATE DATABASE teslafanclub;
+USE teslafanclub;
+create table usuario (
+	idUser int primary key auto_increment,
+    nome varchar(45),
+    username varchar(45),
+    email varchar(60),
+    senha varchar(16),
+    carroFav int,
+    anoNasc int,
+    anoInicio int,
+    foreign key(carroFav) references carros(idCarro)
+);
+create table carros (
+	idCarro int primary key,
+    nomeCarro varchar(45),
+    aceleração0_100 decimal(4,2),
+    aceleração400m decimal(5,2),
+    vMax int,
+    chargeRange int,
+    peso decimal(7,2),
+    potencia int
+);
+INSERT INTO carros VALUES 
+(1, 'Model S', 1.99, 9.23, 320, 624, 2162, 1020),
+(2, 'Model 3', 3.1, 11.6, 260, 504, 1844, 480),
+(3, 'Model X', 2.5, 9.9, 261, 544, 2445, 1020),
+(4, 'Model Y', 3.5, 12.4, 248, 485, 2003, 456),
+(5, 'Roadster', 1.9, 8.8, 400, 1000, 1305, 1020),
+(6, 'Semi', 20, null, 130, 810, 11340, 1000),
+(7, 'Cybertruck', 4.5, 10.8, 200, 480, 2950, 800);
+select * from carros;
